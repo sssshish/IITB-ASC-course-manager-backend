ccb0e839d33be0f8512c38ebdfe71dc27a071acb
sssshish <shivansi57@gmail.com>
2025-06-13 22:11:14 +0530

    Backend working

--- courses_backend/urls.py
+++ courses_backend/urls.py
@@ -16,8 +16,8 @@ router.register(r"courses", CourseViewSet, basename="course")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    path("api/courses/<int:pk>", course_detail),
-    path("api/instances", CourseInstanceCreateView.as_view()),
-    path("api/instances/<int:year>/<int:semester>", list_instances_by_year_sem),
-    path("api/instances/<int:year>/<int:semester>/<int:course_id>", instance_detail),
+    path("api/courses/<int:pk>/", course_detail),
+    path("api/instances/", CourseInstanceCreateView.as_view()),
+    path("api/instances/<int:year>/<int:semester>/", list_instances_by_year_sem),
+    path("api/instances/<int:year>/<int:semester>/<int:course_id>/", instance_detail),
 ]
